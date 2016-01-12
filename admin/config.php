cd9432637d1109d23d100e3d304930777228deeb
Max <maxxl2012@gmail.com>
2016-01-12 12:00:01 +0200

    FirstCommit
    Divotek / VIF - интернет католог

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://192.168.2.86/VIF/admin/');
+define('HTTP_CATALOG', 'http://192.168.2.86/VIF/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://192.168.2.86/VIF/admin/');
+define('HTTPS_CATALOG', 'http://192.168.2.86/VIF/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/VIF/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/VIF/system/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/VIF/admin/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/VIF/admin/view/template/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/VIF/system/config/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/VIF/image/');
+define('DIR_CACHE', 'C:/xampp/htdocs/VIF/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/VIF/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/VIF/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/VIF/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/VIF/system/storage/upload/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/VIF/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'stcart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://192.168.2.86/VIF/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://192.168.2.86/VIF/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/VIF/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/VIF/system/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/VIF/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/VIF/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/VIF/system/config/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/VIF/image/');
+define('DIR_CACHE', 'C:/xampp/htdocs/VIF/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/VIF/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/VIF/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/VIF/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/VIF/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'stcart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
